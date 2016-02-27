935869c4a41465b26a185d2a10f47ca68b783db2
Travis Teo <scenzz@gmail.com>
2016-02-27 08:29:07 +0000

    day 3

---
+++ app/controllers/grams_controller.rb
@@ -0,0 +1,24 @@
+class GramsController < ApplicationController
+  def index
+    @grams = Gram.all
+  end
+  def new
+    @gram = Gram.new
+  end
+  def create
+    @gram = Gram.new(params.require(:gram).permit(:title, :content, :country_code))
+    if @gram.save
+      redirect_to @gram
+    else
+      render :new
+    end
+  end
+  def show
+    @gram = Gram.find(params[:id])
+  end
+  def destroy
+    @gram = Gram.find(params[:id])
+    @gram.destroy
+    redirect_to grams_path
+  end
+end
