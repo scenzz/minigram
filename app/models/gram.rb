877ded2a00430dca0ce6ccbc13b99c64357f37ed
Travis Teo <scenzz@gmail.com>
2016-03-05 08:14:17 +0000

    add

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,5 +1,5 @@
 class Gram < ActiveRecord::Base
   belongs_to :user
-  validates_presence_of :title, :content
+  validates_presence_of :title, :content, :image
   mount_uploader :image, ImageUploader
 end
